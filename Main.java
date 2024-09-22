58efeb385e8ed5eb284f6c9471e20b0a2e952dcf
Anate Muhammed <anatemuhammed@gmail.com>
2024-09-22 19:28:17 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int x= 23;
+        int y = 32;
+        int z= x+(y*x);
+        System.out.print("This is the value of Z");
+        System.out.println(z);
+    }
+}
